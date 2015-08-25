26da52c4e2023e390ff0d2e4b5403341d3a46417
Mic <mickyy92@yahoo.it>
2015-08-25 19:24:36 +0200

    Create TextEMain.java

---
+++ TextEMain.java
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+// Name:        Basic text editor written in Java
+// Purpose:     
+//
+// Created:     12/10/2014
+// Copyright:   (c) Copyright Mic 2014
+// Licence:     GNU GPL
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Main class: contains and runs the main
+
+public class TextEMain 
+{
+	public static void main(String[] args)
+	{
+		UserInterface w = new UserInterface();
+	}
+}
